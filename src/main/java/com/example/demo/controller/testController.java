0aa2e9db4c4cb247ec1b30f3cd3ab05510afa9c4
rkdwlss2 <rkdwlss2@gmail.com>
2023-03-29 18:00:52 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/testController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping("/test")
+    private String test(){
+        return "test";
+    }
+}

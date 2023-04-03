4e08b446bef34027a37af4b0332e1a1bf69d5f92
rkdwlss2 <rkdwlss2@gmail.com>
2023-04-03 15:49:15 +0900

    test rollback

--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -11,53 +11,5 @@ public class testController {
         return "test";
     }
 
-    @GetMapping("/test2")
-    private String test2(){
-        return "test2";
-    }
 
-    @GetMapping("/test3")
-    private String test3(){
-        return "test3";
-    }
-
-    @GetMapping("/test4")
-    private String test4(){
-        return "test4";
-    }
-
-    @GetMapping("/test5")
-    private String test5(){
-        return "test5";
-    }
-
-    @GetMapping("/test6")
-    private String test6(){
-        return "test6";
-    }
-
-
-    @GetMapping("/test7")
-    private String test7(){
-        return "test7";
-    }
-
-    @GetMapping("/test8")
-    private String test8(){
-        return "test8";
-    }
-
-    @GetMapping("/test9")
-    private String test9(){
-        return "test9";
-    }
-    @GetMapping("/test10")
-    private String test10(){
-        return "test10";
-    }
-
-    @GetMapping("/test11")
-    private String test11(){
-        return "test11";
-    }
 }
